d22478e2904c64b95e8972fc03cf28787e1646cd
kgideon <kgideon.dhfs@gmail.com>
2019-04-07 15:10:06 -0400

    Russian roulette mostly functional, Each side can take turns with it keeping track of who is still alive and whose turn it is

--- src/App.js
+++ src/App.js
@@ -7,9 +7,30 @@ import GameButton from './Components/GameButton/GameButton';
 
 import './App.css';
 
-class App extends Component {
+// Initialized states for the game
+const initialState = {
+  playerAlive: true,
+  tomAlive: true,
+  playerTurn: true,
+}
 
-  // Creates a revolver with 6 chambers in the cylinder, and one round loaded randomly. This can also be used to respin the cylinder on a "draw".
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  // Plays the game
+  onGameStart = () => {
+    let gunFired = this.pullTrigger();
+    if (gunFired === true) {
+      alert(`Bang! -- Player Turn: ${this.state.playerTurn}, Player Alive: ${this.state.playerAlive}, Tom Alive: ${this.state.tomAlive}`)
+    } else {
+      alert(`Click. -- Player Turn: ${this.state.playerTurn}, Player Alive: ${this.state.playerAlive}, Tom Alive: ${this.state.tomAlive}`)
+    }
+  }
+
+  // Creates a revolver with 6 chambers in the cylinder, and one round loaded randomly.
   setupRevolver = () => {
     let revolverChambers = [0,0,0,0,0,0];
     let chamberSelection = Random.int(0, 5);
@@ -17,21 +38,26 @@ class App extends Component {
     return revolverChambers;
   }
 
-  // Simple check to see if the chamber is loaded upon trigger pull. If it is, game is over win/loss. If it's empty, the chamber is cycled forward.
-  pullTrigger = (revolver, currentChamber) => {
-    if (revolver[currentChamber] === 1) {
-      alert(`${revolver} -- Bang!`);
-    } else {
-      currentChamber++;
-      alert(`${revolver} -- Click!`);
-    }
-  }
-
-  // Initializes the game
-  onGameStart = () => {
+  // Simple check to see if the chamber is loaded upon trigger pull. If it is, game is over win/loss.
+  pullTrigger = () => {
     let revolver = this.setupRevolver();
-    let currentChamber = 0;
-    this.pullTrigger(revolver, currentChamber);
+    if (revolver[0] === 1) {
+      // Checks who to kill
+      if (this.state.playerTurn === true) {
+        this.setState({playerAlive: false});
+      } else {
+        this.setState({tomAlive: false});
+      }
+      return true;
+    } else {
+      // Next person's turn
+      if (this.state.playerTurn === true) {
+        this.setState({playerTurn: false});
+      } else {
+        this.setState({playerTurn: true});
+      }
+      return false;
+    }
   }
 
   render() {
